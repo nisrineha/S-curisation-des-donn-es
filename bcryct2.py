48858b4ecb9a7e0282f92de84ea4cb75fbc13a39
Nisrine Hammout <nisrinehammout@gmail.com>
2021-03-01 15:46:39 +0100

    Update bcryct2.py

--- bcryct2.py
+++ bcryct2.py
@@ -6,19 +6,16 @@ Created on Sun Feb 14 10:40:23 2021
 """
 import bcrypt
 import redis
-
-#bcrypt
-
+# Definer les dbs
 rserver = redis.StrictRedis(host="localhost", port=6379, db=0, decode_responses=True)
 rsalt = redis.StrictRedis(host="localhost", port=6379, db=1, decode_responses=True)
 
-#def salt(): 
-#    return bcrypt.gensalt()
-
+#fonction cryptage
 def fun_bcrypt(mdp): 
     mdp_hash= bcrypt.hashpw(mdp.encode(), salt= bcrypt.gensalt() )
     return mdp_hash.decode('utf8')
 
+#fonction de verification
 def VerifierMdp(mdp, mdp_hash):     
     return bcrypt.checkpw(mdp.encode('utf8'), mdp_hash.encode('utf8'))
 
