89531a59511139864ae2c975797f8c6afabbc601
Nisrine Hammout <nisrinehammout@gmail.com>
2021-03-01 15:45:20 +0100

    Update aes256.py

--- aes256.py
+++ aes256.py
@@ -2,14 +2,15 @@ import redis
 from Crypto.Cipher import AES
 import base64 
 
-#bcrypt
+# definir les dbs
 rserver = redis.StrictRedis(host="localhost", port=6379, db=0, decode_responses=True)
 rsalt_global = redis.StrictRedis(host="localhost", port=6379, db=2, decode_responses=True)
 rnonce = redis.StrictRedis(host="localhost", port=6379, db=3, decode_responses=True)
-
+# definir la clé 
 key = b"This_key_for_demo_purposes_only!"
 rsalt_global.set('key', key)
 
+# fonction cryptage
 def fun_aes(mdp):
     cipher = AES.new(key, AES.MODE_CTR)
     mdp_cipher = cipher.encrypt( mdp.encode())
@@ -17,6 +18,7 @@ def fun_aes(mdp):
     mdp_aes= base64.b64encode(mdp_cipher).decode('utf-8')
     return mdp_aes , nonce
 
+#fonction decryptage
 def decrypAES(mdp, mdp_hash, nonce):
     
     mdp_decode= base64.b64decode(mdp_hash)
@@ -26,6 +28,4 @@ def decrypAES(mdp, mdp_hash, nonce):
     mdp_decrypt= cipher.decrypt(mdp_decode)    
     return mdp_decrypt== mdp.encode()
     
-    #return bcrypt.checkpw(mdp.encode('utf8'), mdp_decrypt)
-
-    mdp_hash= rserver.get()
+    
